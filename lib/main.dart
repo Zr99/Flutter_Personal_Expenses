0f3fb4df16e2213a2aa878160c9d088a7af885aa
Ryan Ling <Ryan.Ling@metalform.co.nz>
2022-03-14 10:42:23 +1300

    Polish up everything make sure basic features are working

--- lib/main.dart
+++ lib/main.dart
@@ -87,6 +87,12 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  void _deleteTransaction(String id) {
+    setState(() {
+      _userTransactions.removeWhere((tx) => tx.id == id);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
           //mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Chart(_recentTransactions),
-            TransactionList(_userTransactions),
+            TransactionList(_userTransactions, _deleteTransaction),
           ],
         ),
       ),
